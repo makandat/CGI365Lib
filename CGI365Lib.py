e65a34ad23750d611f0c1cf9f9c4cbae380a8378
makandat@japan <tadnakam@hotmail.com>
2023-02-22 07:56:33 +0900

    v1.5.0

--- CGI365Lib.py
+++ CGI365Lib.py
@@ -1,4 +1,4 @@
-# CGI356Lib.py v1.2.2  2023-02-14
+# CGI356Lib.py v1.3.0  2023-02-17
 import os, sys, datetime, io, re
 import urllib.parse
 import pathlib
@@ -44,7 +44,8 @@ class Request:
     # Windows の場合はデフォルトの文字コードが Shift JIS なので、これがないと文字化けする。
     if os.name == 'nt':
       sys.stdin= io.TextIOWrapper(sys.stdin.buffer, encoding=ENC)
-    self.RawData = ""                 #  受け取った生データ
+    self.RawData = ""                 #  受け取った生データ (文字列)
+    self.RawBytes = ""                #  受け取った生データ (バイト列)
     self.Query = self._getQuery()     # GET パラメータ 辞書
     self.Form = self._getForm()       # POST パラメータ 辞書
     self.Method = self._getMethod()   # HTTP メソッド 'GET', 'POST'...
@@ -67,39 +68,82 @@ class Request:
       bstr = self.RawData
     if bstr.startswith("b'"):
       bstr = bstr[2:].strip("'")
-      bstr = bstr.replace("\\n", "\n").replace("\\\\", "\\")
+      bstr = bstr.replace("\\n", "\n").replace("\\r", "\r").replace("\\\\", "\\")
     else:
       pass
     return bstr
 
   # FormData 文字列から変数名をキーとする辞書を作成する。
   def parseFormData(self, s=""):
+    state = 0
     name =""
+    filename = ""
+    octedstream = ""
     result = dict()
+    # s がデフォルトの場合
     if s == "":
       s = self.RawData
-    lines = s.split("\\r\\n")
+    # b'...' を外す。
+    s = self.btos(s)
+    lines = s.split("\r\n")
+    # 行ごと処理を行う。
     for line in lines:
-      if line.startswith("b'-----") or line.startswith("-----") or line == "":
-        pass
-      elif "name=" in line:
-        m = line.split("name=\"")
-        m2 = m[1].split("\"")
-        name = m2[0]
+      # state == 0 のとき、行に "Content-Disposition: form-data;" 見つかるまで読み飛ばす。
+      if state == 0:
+        if "Content-Disposition: form-data;" in line:
+          m = line.split("name=\"")
+          m2 = m[1].split("\"")
+          name = m2[0]
+          if "filename=" in line:
+           # "filename=" が行に含まれるとき、filename を取得。
+            m = line.split("filename=\"")
+            m2 = m[1].split("\"")
+            filename = m2[0]
+            state = 1
+          else:
+            # "filename=" が含まれていないときは、input[type="file"] 以外
+            state = 3
+        else:
+          continue  # それ以外の行は読み飛ばす。
+      elif state == 1:
+        # filename が空でないとき、Content-Type: application/octet-stream が含まれているか？
+        if line == "":
+              continue
+        if filename == "":
+          # filename == "" のときは、name に対する値を "" として、次の form-data まで読み飛ばす。
+          result[name] = ""
+          state = 0
+        else:
+          if 'Content-Type: application/octet-stream' in line:
+            # アップロードされたファイル内容がある場合、次の行でその内容を取得する。
+            state = 2
+      elif state == 2:
+        # ファイル内容(octed-stream)を取得する
+        if line == "":
+          continue
+        octedstream = self.unescape(line)
+        result[name] = [filename, octedstream]
+        name = ""
+        filename = ""
+        state = 0  # ファイル処理終了
+      elif state == 3:
+        # input[type="file"] 以外の場合は空行を読み飛ばし、最初の非空行が値になる。
+        if line == "":
+          continue
+        result[name] = line
+        name = ""
+        state = 0  # 一般コントロール処理終了
       else:
-        if line != "'":
-          value = line
-          result[name] = line
+        pass
     return result
 
   # クライアントから受け取った生データ文字列をJSON形式とみなし変数名をキーとする辞書を作成する。
   def parseJSON(self, s=""):
-    result = dict()
     if s == "":
       s = self.RawData
     s = self.btos(s)
-    obj = json.loads(s)
-    return obj
+    result = json.loads(s)
+    return result
 
   # %xx 文字を元の文字に戻す。
   def fromPercent(self, ps="", plus=True):
@@ -117,15 +161,31 @@ class Request:
     else:
       return urllib.parse.quote(ps)
 
+  # エスケープ文字を元に戻す。
+  def unescape(self, s):
+    #s = s.fromPercent(s)
+    result = s.replace("\\n", "\n") #.replace("\\t", "\t").replace("\\\\", "\\")
+    return result
+  
+  # フォームデータを文字列にする。(デバッグ用)
+  def formdataToString(self):
+    s = ""
+    for k, v in self.Form.items():
+      if isinstance(v, dict):
+        s += f"filename={v[0]}, octed-stream={v[1]};\n"
+      else:
+        s += f"{k}:{v[0]};\n"
+    return s
+  
   # 環境変数 QUERY_STRING から変数名をキーとする辞書を作成する。
   def _getQuery(self):
     result = dict()
     try:
-      qs = os.environ["QUERY_STRING"]
-      if isDebug() and qs != "":
+      self.RawBytes = os.environ["QUERY_STRING"]
+      if isDebug() and self.RawBytes != "":
         self.Method = "GET"
-      self.RawData = qs
-      params = urllib.parse.parse_qs(qs)
+      self.RawData = self.fromPercent(self.RawBytes)
+      params = urllib.parse.parse_qs(self.RawBytes)
       for key in params:
         result[key] = params[key][0]
     except Exception as e:
@@ -135,23 +195,24 @@ class Request:
   # 標準入力から変数名をキーとする辞書を作成する。
   def _getForm(self):
     result = dict()
+    bs = bytes()
     try:
       if isDebug():
         print("Enter form data > ")
         self.Method = 'POST'
-        s = input()
+        self.RawData = input()
       else:
-        s = str(sys.stdin.buffer.read())
-      self.RawData += s
-      #info(s)
-      if s.startswith("b'-----"):
+        bs = sys.stdin.buffer.read()
+      if self.RawData == "":
+        self.RawData += bs.decode()
+        self.RawBytes = bs
+      s = self.RawData
+      if s.startswith("-----"):
         # FormData
         result = self.parseFormData(s)
       else:
         # その他
         result = urllib.parse.parse_qs(s)
-        for key in ps:
-          result[key] = ps[key][0]
     except Exception as e:
       pass
     return result
@@ -206,7 +267,7 @@ class Response:
     return
 
   # クッキーをセットする。cookies は辞書オブジェクト。
-  def setCookie(self, cookies):
+  def setCookie(self, cookies:dict):
     for key, value in cookies.items():
       self.Cookie.append(f"{key}={value}")
     return
@@ -221,10 +282,10 @@ class Response:
     return buff
 
   # 文字列を応答として返す。s はその文字列。
-  def sendString(self, s, mime=f'text/html; charset={ENC}', cookie=True, embed=None):
+  def sendString(self, s:str, mime=f'text/html; charset={ENC}', cookie=True, embed=None):
     if isinstance(embed, dict):
       for k, v in embed.items():
-         s = s.replace("{{" + k + "}}", v)
+         s = s.replace("{{ " + k + " }}", v)
     buff = ""
     if mime == "text":
       mime = f"Content-Type: text/plain; charset={ENC}"
@@ -240,13 +301,19 @@ class Response:
       pass
     if cookie:
       buff = self.makeCookie()
-    buff += mime + "\n\n" + s
+    buff += "Content-Type: " + mime + "\n\n" + s
     print(buff)
     return
 
   # 単純な文字列を返す。
-  def sendSimple(self, s):
+  def sendSimple(self, s:str):
     print("Content-Type: text/plain\n\n" + s)
+    return
+
+  # pprint で単純な文字列を返す。(for Debug)
+  def sendPPrint(self, s:str):
+    print("Content-Type: text/plain\n")
+    pprint(s)
     return
 
   # バイナリーデータを返す。
@@ -376,3 +443,25 @@ class Response:
       s += "\n"
     s += "\n"
     print(s)
+    return
+
+#  ユーティリティ
+class CGIUtility:
+  # HTML テーブルを作成する。
+  @staticmethod
+  def htmlTable(table="", tr="", th="", td=""):
+    return
+
+  # HTML リストを作成する。
+  @staticmethod
+  def htmlList(list="ul", ul="", li=""):
+    return
+  
+  # SVG を作成する。(円 'circle' と正方形 'square' のみ)
+  @staticmethod
+  def svg(shape="circle", size=32, borderWidth=1, borderColor="black", bgcolor="white"):
+    return
+
+  
+
+  
